62d69f5dfecfbaee2106b1b2dadda8f19bece649
Ricky Chen <rickyshen1991@hotmail.com>
2016-03-23 05:13:50 +0000

    role fxn

---
+++ DiplomaOptions/OptionsWebSite/Controllers/RolesController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using OptionsWebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OptionsWebSite.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RolesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Roles
+        public ActionResult Index()
+        {
+            var roles = db.Roles.ToList();
+            return View(roles);
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IdentityRole role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            var roleCollection = collection["Name"];
+
+            if (collection["Name"] == "")
+            {
+                ViewBag.ResultMessage = "Role name cannot be empty!";
+                return View("Index", db.Roles.ToList());
+            }
+
+            try
+            {
+                db.Roles.Add(new IdentityRole()
+                {
+                    Name = collection["Name"]
+                });
+                db.SaveChanges();
+                ViewBag.Msg = "Role created successfully !";
+                return View("Index", db.Roles.ToList());
+            }
+            catch
+            {
+                ViewBag.Msg = "Cannot have duplicate roles";
+                return View("Index", db.Roles.ToList());
+            }
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(string name)
+        {
+            var role = db.Roles.Where(r => r.Name.ToLower() == name.ToLower()).FirstOrDefault();
+            return View(role);
+        }
+
+        // POST: Roles/Edit/5
+        [HttpPost]
+        public ActionResult Edit(IdentityRole roleName)
+        {
+            try
+            {
+                db.Entry(roleName).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(string roleName)
+        {
+            if (roleName == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IdentityRole role = db.Roles.Find(roleName);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string roleName)
+        {
+            try
+            {
+                var thisRole = db.Roles.Where(r => r.Name.Equals(roleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+                db.Roles.Remove(thisRole);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
