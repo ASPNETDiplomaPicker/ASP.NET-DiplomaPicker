86e38d0215f461931f94e4d8e1feb27af6587fc4
hsuanweifu <hsuanweifu@gmail.com>
2016-03-26 00:39:45 +0000

    client

---
+++ DiplomaOptions/OptionsClient/js/OptionController.js
@@ -0,0 +1,56 @@
+﻿(function () {
+
+    var app = angular.module("diplomaPicker");
+    var url = "http://localhost:51290/api/";
+    var DisplayPick = function ($scope, $http) {
+        // get user id
+        // get year and term
+        $http.get(url.concat("YearTerms"))
+            .then(function (response) {
+                var yearTerm;
+                response.data.forEach(function (x) {
+                    if (x.isDefault) {
+                        yearTerm = x;
+                    }
+                });
+                if (yearTerm.Term == 10) {
+                    yearTerm.Term = "Winter";
+                }
+                else if (yearTerm.Term == 20) {
+                    yearTerm.Term = "Spring/Summer";
+                }
+                else if (yearTerm.Term == 30) {
+                    yearTerm.Term = "Fall";
+                }
+                $scope.yearTerm = yearTerm;
+            });
+        // get avaliable options 
+        $http.get(url.concat("Options"))
+            .then(function (response) {
+                var array = [];
+                response.data.forEach(function (x) {
+                    if (x.isActive) {
+                        array.push(x);
+                    }
+                });
+                $scope.options = array;
+            });
+    }
+
+    app.controller("DisplayPick", DisplayPick);
+}());
+
+(function () {
+    var app = angular.module("diplomaPicker");
+    var url = "http://localhost:51290/api/";
+
+    var PostPick = function ($scope, $http) {
+        $scope.createChoice = function (user) {
+            window.alert(user.YearTermId);
+            window.alert(user.StudentFirstName);
+            //$http.post("http://w10.hsuanweifu.info/api/Students", angular.copy(student));
+        }
+    }
+
+    app.controller("PostPick", PostPick);
+}());
---
+++ DiplomaOptions/OptionsClient/js/route.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    var app = angular.module("diplomaPicker", ["ngRoute"]);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "view/login.html",
+                controller: "Login"
+            })
+            .when("/register", {
+                templateUrl: "view/register.html",
+                controller: "CreateUser"
+            })
+            .when("/pick", {
+                templateUrl: "view/pick.html",
+                controller: "DisplayPick"
+            })
+            .when("/summary", {
+                templateUrl: "view/summary.html",
+                controller: "CreateController"
+            })
+            .otherwise({
+                redirectTo: "/login"
+            });
+    });
+}());
